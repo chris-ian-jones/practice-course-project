b65f11cecc20513826af29a57f1c85fafde8786d
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-29 18:01:07 +0100

    WIP: setup styling and mobile responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CourseListComponent } from './components/course-list/course-list.component';
 import { CourseItemComponent } from './components/course-item/course-item.component';
 import { MaterialModule } from './material/material.module';
+import { CourseListHeroComponent } from './components/course-list-hero/course-list-hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    CourseItemComponent
+    CourseItemComponent,
+    CourseListHeroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/course-list-hero/course-list-hero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-list-hero',
+  templateUrl: './course-list-hero.component.html',
+  styleUrls: ['./course-list-hero.component.css']
+})
+export class CourseListHeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
